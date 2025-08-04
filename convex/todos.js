ba0a2285956c9557fa1a568a70c1a94cde9b446e
shahirsizan <shahir.sizan18@gmail.com>
2025-08-04 21:02:45 +0600

    Implemented Convex API `deleteTodo` to delete an existing todo

--- convex/todos.js
+++ convex/todos.js
@@ -32,3 +32,10 @@ export const toggleTodo = mutation({
 		await ctx.db.patch(args.id, { isCompleted: !todo.isCompleted });
 	},
 });
+
+export const deleteTodo = mutation({
+	args: { id: v.id("todos") },
+	handler: async (ctx, args) => {
+		await ctx.db.delete(args.id);
+	},
+});
